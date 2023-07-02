d2f6847b29a14adaf6ee124743544b6149ba0993
kaslota <kyljas0510@gmail.com>
2023-07-02 10:28:26 +0300

    solved the problem with the description and changed some classes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,13 @@
 // Show current time and day
-
-let now = new Date();
+let date = new Date();
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let days = [
   "Sunday",
   "Monday",
@@ -10,23 +17,15 @@ let days = [
   "Friday",
   "Saturday",
 ];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let currentTime = document.querySelector("#current-time");
-currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+let day = days[date.getDay()];
+let time = document.querySelector("#current-time");
+time.innerHTML = `${day} ${hours}:${minutes}`;
 
 // Show forecast html and data
 function transformOnDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
@@ -40,14 +39,14 @@ function displayForecast(response) {
       forecast =
         forecast +
         `<div class="col">
-            <div class="another-weather">
-              <div class="another-day"><strong>${transformOnDay(
+            <div class="forecast-weather">
+              <div class="forecast-day"><strong>${transformOnDay(
                 forecastDay.dt
               )}</strong></div>
-              <img class="another-icon" src="https://openweathermap.org/img/wn/${
+              <img class="forecast-icon" src="https://openweathermap.org/img/wn/${
                 forecastDay.weather[0].icon
               }@2x.png" alt="icon">
-              <div class="another-max-min-tem" id="forecast-max-min-tem">
+              <div class="forecast-max-min-tem">
                  ${Math.round(forecastDay.temp.min)} / ${Math.round(
           forecastDay.temp.max
         )}
@@ -55,21 +54,32 @@ function displayForecast(response) {
             </div>
         </div> `;
     }
-  
   });
   forecast = forecast + `</div>`;
   forecastElement.innerHTML = forecast;
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates.lat);
   let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 // Show current input data and auto city(temperature, description, humidity, wind)
+function classDescription() {
+  let descriptionElement = document.querySelector("#description");
+  if (descriptionElement.innerHTML === "clear sky") {
+    descriptionElement.classList.add("clear-sky");
+    descriptionElement.classList.remove("haze");
+  } else if (descriptionElement.innerHTML === "haze") {
+    descriptionElement.classList.add("haze");
+     descriptionElement.classList.remove("clear-sky");
+  } else {
+    descriptionElement.classList.add("description-all");
+    descriptionElement.classList.remove("clear-sky");
+     descriptionElement.classList.remove("haze");
+  }
+}
 
 function showCityData(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
@@ -88,6 +98,7 @@ function showCityData(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+  classDescription();
 
   let inputIcon = response.data.weather[0].icon;
   let icon = document.querySelector("#icon");
@@ -101,6 +112,7 @@ function showCityData(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
   getForecast(response.data.coord);
 }
 function searchCity(city) {
@@ -132,4 +144,3 @@ function askCurrentPosition() {
 
 let currentBtn = document.querySelector("#current-btn");
 currentBtn.addEventListener("click", askCurrentPosition);
-
